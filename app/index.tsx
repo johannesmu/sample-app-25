a251483eaac50915ed862e441632295dfe4a502c
johannes <johannes.muljana@ait.nsw.edu.au>
2025-02-21 11:53:46 +1100

    updated authentication screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { View, Text, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 export default function Login() {
     return(
-        <View>
-            <Text>Login</Text>
-            <Link href="/signup">
-                <Text>Go to Sign up</Text>
-            </Link>
-        </View>
+        <SafeAreaView>
+            <View>
+                <Text>Login</Text>
+                <Link href="/signup">
+                    <Text>Go to Sign up</Text>
+                </Link>
+            </View>
+        </SafeAreaView>
     )
 }
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,12 +1,30 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 export default function Signup() {
     return (
-        <View>
-            <Text>Signup</Text>
-            <Link href="/">
-                <Text>Go to Login</Text>
-            </Link>
-        </View>
+
+        <SafeAreaView style={ styles.container }>
+            <View style={ styles.form }>
+                <Text style={ styles.title }>Sign up for an account</Text>
+                <Link href="/">
+                    <Text>Go to Login</Text>
+                </Link>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e3e8a2"
+    },
+    form: {
+        marginTop: 50,
+    },
+    title: {
+        fontSize: 32,
+        textAlign: "center",
+    },
+})
