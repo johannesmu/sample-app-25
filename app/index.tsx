b1ae33bc2e0def17b4d816f213c34caf203a04ce
johannes <johannes.muljana@ait.nsw.edu.au>
2025-02-28 11:24:08 +1100

    adding signup form

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
 export default function Login() {
     return(
         <SafeAreaView>
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,12 +1,33 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Pressable } from 'react-native'
+import { useState, useEffect } from 'react'
 import { Link } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { FormLabel } from '@/components/FormLabel'
+
 export default function Signup() {
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
     return (
 
         <SafeAreaView style={ styles.container }>
             <View style={ styles.form }>
                 <Text style={ styles.title }>Sign up for an account</Text>
+                <FormLabel color="black" bg="transparent" text="Email" />
+                <TextInput 
+                    style={ styles.input }
+                    value={ email }
+                    onChangeText={ (val) => setEmail(val) }
+                />
+                <FormLabel color="black" bg="transparent" text="Password" />
+                <TextInput 
+                    style={ styles.input }
+                    secureTextEntry={ true }
+                    value={ password }
+                    onChangeText={ (val) => setPassword(val) }
+                />
+                <Pressable style={ styles.button }>
+                    <Text style={ styles.buttonText }>Sign up</Text>
+                </Pressable>
                 <Link href="/">
                     <Text>Go to Login</Text>
                 </Link>
@@ -18,13 +39,31 @@ export default function Signup() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#e3e8a2"
+        backgroundColor: "hsl(64, 60%, 77%)"
     },
     form: {
         marginTop: 50,
+        marginHorizontal: 40,
     },
     title: {
         fontSize: 32,
         textAlign: "center",
     },
+    input: {
+        fontSize: 18,
+        backgroundColor: "white",
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: "black",
+        padding: 5,
+    },
+    button: {
+        marginVertical: 15,
+        padding: 10,
+        backgroundColor: "hsl(64, 60%, 30%)",
+    },
+    buttonText: {
+        textAlign: "center",
+        color: "hsl(64, 60%, 90%)"
+    }
 })
