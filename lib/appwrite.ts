fa153b43bea6854d719f3a4abd12e6b065a6b747
johannes <johannes.muljana@ait.nsw.edu.au>
2025-03-07 10:28:22 +1100

    added appwrite endpoint

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,7 @@
 import { Client, Databases, Account } from "react-native-appwrite"
 
 const client = new Client()
+.setEndpoint("https://cloud.appwrite.io/v1")
 .setProject('67c0e32f002cdb733d1f')
 .setPlatform('au.edu.nsw.ait.testapp');
 
