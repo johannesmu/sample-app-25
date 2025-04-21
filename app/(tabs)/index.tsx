bb68416be78a8620e3197d2d8b05ac020f2f310e
johannes <jmuljana@gmail.com>
2025-04-21 14:22:55 +1000

    code cleanup and added separator for flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,28 +10,32 @@ export default function Home() {
   const[ items, setItems ] = useState([])
   const[ modalVisible, setModalVisible ] = useState<boolean>( false )
   const user = useUser()
-  // useEffect( () => {
-  //   if(!user.current) {
-  //     router.navigate('/signup')
-  //   }
-  // },[user])
-
   const data = useData()
+
   useEffect(
     () => {
       setItems( data.current )
-      console.log(data)
+      console.log( data.current )
     },
     [ data.current ]
   )
+
+  const Separator = () => {
+    return(
+      <View style={ styles.separator }></View>
+    )
+  }
 
   return (
     <View style={ styles.container }>
       <Text>Home</Text>
       <FlatList 
       data={items}
-      renderItem={ ({item}:any) => <ListItem name={item.name} /> }
+      renderItem={ 
+        ({item}:any) => <ListItem name={item.name} id={item.$id }/> 
+      }
       keyExtractor={(item:any) => item.$id }
+      ItemSeparatorComponent={Separator}
       />
       <Modal visible={ modalVisible }>
         <View>
@@ -46,5 +50,10 @@ const styles = StyleSheet.create({
   container: {
       flex: 1,
       backgroundColor: "hsl(64, 60%, 77%)",
-  }
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "hsl(0, 0.00%, 50.60%)",
+
+  },
 })
