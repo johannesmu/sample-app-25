a2d0282c9f9ebbe42824feb0fcfbde5cfb53fc91
johannes <jmuljana@gmail.com>
2025-04-22 09:53:56 +1000

    fixed date display component

--- components/DateDisplay.tsx
+++ components/DateDisplay.tsx
@@ -1,12 +1,22 @@
 import { Text } from "react-native"
 
 export function DateDisplay( props:any ) {
+    // create a date object from timestamp
     const dt = new Date( props.date )
+    // extract the full year eg 2025
     const y = dt.getFullYear()
-    const m = dt.getMonth()
+    // get the month 0-11 for Jan - Dec
+    let m = (dt.getMonth()).toString()
+    const monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    // replace m with month name
+    m = monthNames[ parseInt(m)]
+    // get the date
     const d = dt.getDate()
-    const dy = dt.getDay()
+    // get the day of the week as a number 0-6 for Sunday-Saturday
+    let dy = (dt.getDay()).toString()
+    const dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    dy = dayNames[ parseInt(dy) ]
     return (
-        <Text>{`${dy}, ${dy} ${d}/${m}/${y}`}</Text>
+        <Text>{`${dy}, ${d} ${m} ${y}`}</Text>
     )
 }
