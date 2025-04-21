82207708d687e148ed36b688269d048f8d11f028
johannes <jmuljana@gmail.com>
2025-04-21 14:23:25 +1000

    updated styling for list item

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,8 +1,22 @@
 import { View, Text, StyleSheet } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 export function ListItem ( props:any ) {
     return(
-        <View>
+        <View style={ styles.itemContainer }>
             <Text>{ props.name }</Text>
+            <Text>{ props.id }</Text>
+            <Ionicons name="chevron-forward" size={24}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    itemContainer: {
+        padding: 5,
+        paddingVertical: 15,
+        backgroundColor: "white",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    }
+})
