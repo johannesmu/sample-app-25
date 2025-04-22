5ab47c7be8c97fc861f5f4584c0a6c6fde906c5b
johannes <jmuljana@gmail.com>
2025-04-22 12:25:12 +1000

    fixed logout in profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,31 +1,30 @@
 import { Image, StyleSheet, Platform, View, Text, Pressable } from 'react-native';
 import { useState, useEffect } from 'react'
-// import { useUser } from '@/contexts/UserContext';
+import { useUser } from '@/contexts/UserContext';
 import { router } from 'expo-router';
 
 export default function ProfileScreen() {
-  const[ email, setEmail ] = useState<any>('')
-  // const user = useUser()
-  
-  // useEffect( () => {
-  //   if( user.current ) {
-  //     setEmail( user.current.email )
-  //   }
-  // }, [user])
+  const [email, setEmail] = useState<any>('')
+  const user = useUser()
+
+  useEffect( () => {
+    if( user.current ) {
+      setEmail( user.current.email )
+    }
+  }, [user])
 
   const signOut = async () => {
-    // await user.logout()
+    await user.logout()
     router.navigate("/signup")
   }
   return (
-    <View style={ styles.container}>
-      <Text>Profile</Text>
-      <Text>{ email }</Text>
-      <Pressable onPress={ () => signOut() }>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Profile</Text>
+      </View>
+      <Text>{email}</Text>
+      <Pressable onPress={() => signOut()}>
         <Text>Sign out</Text>
-      </Pressable>
-      <Pressable onPress={ () => router.navigate("/signup") }>
-        <Text>Login Page</Text>
       </Pressable>
     </View>
   );
@@ -35,5 +34,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "hsl(64, 60%, 77%)",
+  },
+  header: {
+    backgroundColor: "#f1f7b7",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 22,
   },
 })
