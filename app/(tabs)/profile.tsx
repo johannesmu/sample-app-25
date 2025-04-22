682788fdaafa3f141d23d1c2c79a03bc2d56d927
johannes <jmuljana@gmail.com>
2025-04-22 18:38:40 +1000

    conditional render

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform, View, Text, Pressable } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, Pressable, ActivityIndicator } from 'react-native';
 import { useState, useEffect } from 'react'
 import { useUser } from '@/contexts/UserContext';
 import { router } from 'expo-router';
@@ -7,9 +7,9 @@ export default function ProfileScreen() {
   const [email, setEmail] = useState<any>('')
   const user = useUser()
 
-  useEffect( () => {
-    if( user.current ) {
-      setEmail( user.current.email )
+  useEffect(() => {
+    if (user.current) {
+      setEmail(user.current.email)
     }
   }, [user])
 
@@ -17,17 +17,28 @@ export default function ProfileScreen() {
     await user.logout()
     router.navigate("/signup")
   }
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Profile</Text>
+  if (email) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Profile</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.title}>Email</Text>
+          <Text style={styles.input}>{email}</Text>
+          <Pressable onPress={() => signOut()}>
+            <Text>Sign out</Text>
+          </Pressable>
+        </View>
+
       </View>
-      <Text>{email}</Text>
-      <Pressable onPress={() => signOut()}>
-        <Text>Sign out</Text>
-      </Pressable>
+    )
+  }
+  else {
+    <View style={styles.loading}>
+      <ActivityIndicator size={25} />
     </View>
-  );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -43,5 +54,24 @@ const styles = StyleSheet.create({
   },
   headerText: {
     fontSize: 22,
+  },
+  content: {
+    padding: 10,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "hsl(64, 60%, 77%)",
+  },
+  title: {
+    fontSize: 22,
+    marginBottom: 5,
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    backgroundColor: "hsl(64, 60%, 95%)",
+    marginBottom: 10,
   },
 })
