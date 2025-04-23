fc44eb0f4e9c20a6f00249d32dde8048eeb1ae2f
johannes <johannes.muljana@ait.nsw.edu.au>
2025-04-23 15:57:17 +1000

    add formatting to [id] page

--- app/(tabs)/detail/[id].tsx
+++ app/(tabs)/detail/[id].tsx
@@ -1,7 +1,35 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { useLocalSearchParams, Link, useNavigation } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function DetailScreen( props:any ) {
-    return(
-        <View>Detail</View>
+export default function DetailScreen(props: any) {
+    // receive the id in the params
+    const { id } = useLocalSearchParams()
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Link href="/(tabs)">
+                    <Ionicons size={24} name="chevron-back" />
+                    <Text style={styles.headerText}>Return to list</Text>
+                </Link>
+            </View>
+            <Text>Detail for {id}</Text>
+        </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "hsl(64, 60%, 77%)",
+    },
+    header: {
+        padding: 10,
+        backgroundColor: "hsl(64, 60%, 55%)",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    headerText: {
+        fontSize: 22,
+        textAlign: "center",
+    },
+})
