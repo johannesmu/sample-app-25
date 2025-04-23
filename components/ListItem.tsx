706c83cf971c2c258661a0974aa2376df01336ce
johannes <johannes.muljana@ait.nsw.edu.au>
2025-04-23 14:32:54 +1000

    adding formatting to list items

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,8 +1,28 @@
 import { View, Text, StyleSheet } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { Link } from 'expo-router'
+
 export function ListItem ( props:any ) {
     return(
-        <View>
-            <Text>{ props.name }</Text>
-        </View>
+        <Link href={{
+            pathname: "/(tabs)/detail/[id]",
+            params: { id: props.id }
+        }}>
+            <View style={ styles.item }>
+                <Text>{ props.name }</Text>
+                <Ionicons name="chevron-forward" size={25} />
+            </View>
+        </Link>
     )
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: "hsl(64, 60%, 85%)",
+        padding: 15,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+    },
+})
